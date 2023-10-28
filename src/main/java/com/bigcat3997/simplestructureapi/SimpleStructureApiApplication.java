d5e04de51a9f14f8f026e97728c605ea603b2b80
k3rcaft <vqt.develop@gmail.com>
2023-10-28 12:57:06 +0700

    init the project

---
+++ src/main/java/com/bigcat3997/simplestructureapi/SimpleStructureApiApplication.java
@@ -0,0 +1,13 @@
+package com.bigcat3997.simplestructureapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleStructureApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleStructureApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bigcat3997/simplestructureapi/SimpleStructureApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigcat3997.simplestructureapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleStructureApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
